41b9d5a9d3995bd6b5fc862d1abf565b651faa3b
Brandon <brandon@melissa-brandon.com>
2011-01-07 22:56:23 -0800

    Ajax Loading

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,16 @@
 $(function(){
 	
-	$('#all-staff-list').infinitescroll({
-	    navSelector  : "div.pagination",   // selector for the paged navigation (it will be hidden)
-	    nextSelector : "div.pagination a:first", // selector for the NEXT link (to page 2)
-	    itemSelector : "div.single-user", // selector for all items you'll retrieve
-		loadingText : "",
-		animate		: false,
-		donetext     : "Nice work!  You have met everyone!",
-		loadingImg	: "/images/loading.gif"
-	});
+	if($('#all-staff-list').html())
+	{
+		$('#all-staff-list').infinitescroll({
+		    navSelector  : "div.pagination",   // selector for the paged navigation (it will be hidden)
+		    nextSelector : "div.pagination a:first", // selector for the NEXT link (to page 2)
+		    itemSelector : "div.single-user", // selector for all items you'll retrieve
+			loadingText : "",
+			animate		: false,
+			donetext     : "Nice work!  You have met everyone!",
+			loadingImg	: "/images/loading.gif"
+		});
+	}
 	
 });
