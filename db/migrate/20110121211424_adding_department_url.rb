e0073ea130d191bc7f716ee483d4be3d5044e8df
brandon <brandon@bethel-churchs-imac.local>
2011-01-21 13:17:15 -0800

    Migration

--- db/migrate/20110121211424_adding_department_url.rb
+++ db/migrate/20110121211424_adding_department_url.rb
@@ -1,6 +1,9 @@
 class AddingDepartmentUrl < ActiveRecord::Migration
   def self.up
     add_column :departments, :cached_slug, :string
+    
+    # This will generate the URL
+    Department.all.each { |department| department.save }
   end
 
   def self.down
