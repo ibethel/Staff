b0275779ff8accf9ce9874bd77bfaf67a2363c81
brandon <brandon@bethel-churchs-imac.local>
2011-01-20 16:03:16 -0800

    Staff

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   
   def departments
-    ["Web", "Accounting", "Jesus Culture"].sort
+    ["Web", "Accounting", "Jesus Culture", "Graphics", "Manufacturing", "Video", "Global Legacy", "IBSSM", "Human Resources", "Information Technology", "Customer Support", "Transformation Center", "BCS", "School of Worship", "Missions", "Music", "Marketing", "Eagles Nest", "Moral Revolution", "Media", "Office Staff", "Maintenance", "Pastor on Call", "Youth", "Iris Ministries"].sort
   end
   
 end
